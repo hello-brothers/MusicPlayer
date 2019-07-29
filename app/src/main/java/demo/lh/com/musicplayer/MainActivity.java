1e6053d0bb76d83c2da934ca779d8fa463b8ae8c
hellobrothers <1479784191@qq.com>
2019-07-29 18:27:55 +0800

    添加资源

--- app/src/main/java/demo/lh/com/musicplayer/MainActivity.java
+++ app/src/main/java/demo/lh/com/musicplayer/MainActivity.java
@@ -2,12 +2,29 @@ package demo.lh.com.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mucis, menu);
+        return true;
     }
 }
